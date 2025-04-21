0842c949a08264e7362137d64c26d0d1cc2b5824
Kan <ssitinan@gmail.com>
2025-04-21 18:08:03 +0700

    อันนี้ใช้ได้นะ

--- db.js
+++ db.js
@@ -1,15 +1,16 @@
 const mongoose = require('mongoose');
 
 const sensorDataSchema = new mongoose.Schema({
-  pm2_5: { type: Number, required: true },  // ปรับชื่อให้ตรงกับคีย์จาก MQTT
+  pm2_5: { type: Number, required: true },
   pm10: { type: Number, required: true },
   pm1_0: { type: Number, required: true },
   CO: { type: Number, required: true },
   O3: { type: Number, required: true },
   NO2: { type: Number, required: true },
   SO2: { type: Number, required: true },
-  timestamp: { type: Date, default: Date.now },
+  Predicted_AQI: { type: Number, required: false }
 });
+
 
 const SensorData = mongoose.model('SensorData', sensorDataSchema);
 
